ff5bab94780413e122a4d29647e517e5a363f64e
Anton Grigorash <sf13371@yandex.ru>
2022-09-15 18:53:11 +0300

    feat: full adaptive for about and buy nootris blocks

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,17 @@
+import Navigation from "../Navigation/Navigation";
+
 /* eslint-disable jsx-a11y/anchor-is-valid */
 function Header() {
   return (
     <header className="header">
-      <p className="header__logo"></p>
-      <nav className="header__navigation">
-        <a href="#" className="header__nav-link">
-          FAQ
-        </a>
-        <a href="#" className="header__nav-link">
-          Оплата и доставка
-        </a>
-        <a href="#" className="header__nav-link">
-          Возврат
-        </a>
-        <a href="#" className="header__nav-link">
-          Исследования
-        </a>
-        <a href="#" className="header__nav-link">
-          Личный кабинет
-        </a>
-        <a href="tel:8800600-09-90" className="header__nav-link">
-          8 8 (800) 600-09-90
-        </a>
-      </nav>
-      <button className="header__basket"></button>
+      <div className="header__container">
+        <p className="header__logo"></p>
+        <Navigation />
+        <div className="header__buttons">
+          <button className="header__basket" />
+          <button className="header__burger" />
+        </div>
+      </div>
     </header>
   );
 }
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,27 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+function Navigation() {
+  return (
+    <nav className="navigation">
+      <a href="#" className="navigation-link">
+        FAQ
+      </a>
+      <a href="#" className="navigation-link">
+        Оплата и доставка
+      </a>
+      <a href="#" className="navigation-link">
+        Возврат
+      </a>
+      <a href="#" className="navigation-link">
+        Исследования
+      </a>
+      <a href="#" className="navigation-link">
+        Личный кабинет
+      </a>
+      <a href="tel:8800600-09-90" className="navigation-link">
+        8 8 (800) 600-09-90
+      </a>
+    </nav>
+  );
+}
+
+export default Navigation;
